ebb8f597e84932912eefa1441df34a378a7ef990
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2024-07-14 19:25:26 +0100

    api url updated

--- src/hooks/useCustomers.ts
+++ src/hooks/useCustomers.ts
@@ -2,7 +2,7 @@ import { useEffect, useReducer, useState} from "react";
 import { Customers } from "../context/app-context-type";
 import useFetch from "./useFetch";
 
-const global_URL = 'http://127.0.0.1:55721';
+const global_URL = 'http://127.0.0.1:62341';
 
 type reducerAction = {
     type: string,
